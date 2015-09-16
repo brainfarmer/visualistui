c778cdfd9bd79c163c4fe11c100c364d55891fca
jszod <joe.szod@gmail.com>
2015-09-16 17:41:38 -0400

    Using material icon fonts, basic side nav show/hide

--- app/app.js
+++ app/app.js
@@ -3,10 +3,18 @@
 // Declare app level module which depends on views, and components
 angular.module('visualistApp', [
   'ngRoute',
+  'ngMaterial',
   'visualistApp.project',
   'visualistApp.view2',
   'visualistApp.version'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/project'});
+}])
+
+.controller('AppController', ['$scope', '$mdSidenav', function($scope, $mdSidenav) {
+  $scope.toggleSidenav = function() {
+    // $mdSidenav(menuId).toggle();
+    $mdSidenav('left').toggle();
+  };
 }]);
